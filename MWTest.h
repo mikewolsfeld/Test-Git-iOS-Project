82e8bf6fefc7f24ddd1344a2f30f47e602596f76
Mike Wolsfeld <mikewolsfeld@Mikes-Mac.local>
2014-03-01 11:24:46 -0800

    added a test class

---
+++ MWTest.h
@@ -0,0 +1,13 @@
+//
+//  MWTest.h
+//  Git Test
+//
+//  Created by Mike Wolsfeld on 3/1/2014.
+//  Copyright (c) 2014 Mike Wolsfeld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWTest : NSObject
+
+@end
---
+++ MWTest.m
@@ -0,0 +1,13 @@
+//
+//  MWTest.m
+//  Git Test
+//
+//  Created by Mike Wolsfeld on 3/1/2014.
+//  Copyright (c) 2014 Mike Wolsfeld. All rights reserved.
+//
+
+#import "MWTest.h"
+
+@implementation MWTest
+
+@end
